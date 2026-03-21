981b6415af06ae7ce1474bb09315b0a475842a75
Your <yrukshan345@gmail.com>
2026-03-21 14:34:49 +0530

    Backend Update Commit

--- leave-management-backend/database/db.go
+++ leave-management-backend/database/db.go
@@ -2,15 +2,36 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func Connect() {
-	connStr := "host=localhost port=5432 user=postgres password=Root01 dbname=leave_system sslmode=disable"
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
+	// Read env variables
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	// Build connection string
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode,
+	)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
